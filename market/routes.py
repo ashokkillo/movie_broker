745ffc954edc5ad1d9175c9f0a36df45410b3a14
ashokkillo <ashok.killo@gmail.com>
2023-05-29 11:01:40 +0530

    P9.Flask Forms

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market.models import Item , User
 from market.forms import RegisterForm, LoginForm
+from market import db
 
 @app.route('/')
 @app.route('/home')
@@ -11,20 +12,26 @@ def home_page():
 @app.route('/market')
 def market_page():
     items = Item.query.all()
-#     [
-#     {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-#     {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-#     {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-# ]
     return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(
+            username= form.username.data,
+            email_address = form.email_address.data,
+            password_hash = form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors !={}: #If there are no error in forms
+        for err_msg in form.errors.values():
+            print(f'There was an error in creating an user : {err_msg}')
     return render_template('register.html', form=form)
 
 
 @app.route('/login')
 def login_page():
-    form = LoginForm()
+    form = LoginForm()    
     return render_template('login.html', form=form)
